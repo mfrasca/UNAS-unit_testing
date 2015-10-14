16bd3c0dc33e55b88c403d82f70000d33152d4f4
Mario Frasca <mario@anche.no>
2015-10-14 13:48:47 -0500

    preparing the task for Wednesday October 14th.

---
+++ test/testglobals.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# copyright by its authors
+#
+# This file is part of the didactic code used at the UNAS
+#
+# UNAS didactic code is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# UNAS didactic code is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with UNAS didactic code. If not, see <http://www.gnu.org/licenses/>.
+
+
+# este archivo contiene las pruebas unitarias para las funciones globales
+# definidas en los módulos.
+
+
+import unittest
+
+
+class GlobalTests(unittest.TestCase):
+
+    def test_mergevalues_equal(self):
+        'if the values are equal, return it'
+        1/0
+
+    def test_mergevalues_conflict(self):
+        'if they conflict, return both'
+        1/0
+
+    def test_mergevalues_one_empty(self):
+        'if one is empty, return the non empty one'
+        1/0
+
+    def test_mergevalues_both_empty(self):
+        'if both are empty, return the empty string'
+        1/0
---
+++ unas/globfunc.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# copyright by its authors
+#
+# This file is part of the didactic code used at the UNAS
+#
+# UNAS didactic code is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# UNAS didactic code is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with UNAS didactic code. If not, see <http://www.gnu.org/licenses/>.
+
+
+def mergevalues(first, second, merge_conflicts="%s|%s"):
+    """return only value if first == second, or their merged string
+
+    `mergevalues` es una función de tres parámetros, los primeros dos
+    son los valores a juntar, el tercero es el formato en que se
+    combinan los dos valores en el caso de conflicto. la función
+    averigua si los dos primeros argumentos tienen el mismo valor, en el
+    cual caso, este será el valor de la función. si los dos primeros
+    argumentos tienen valor diferentes, en tercer argumento es utilizado
+    para combinarlos.
+
+    """
+
+    pass
